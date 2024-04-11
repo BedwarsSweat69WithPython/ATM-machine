d9a345818e64f18fdfa1a5eb505acd6b2db61cd6
BedwarsSweat69WithPython <166590530+BedwarsSweat69WithPython@users.noreply.github.com>
2024-04-11 12:24:31 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,8 @@
+class atm(object):
+
+    def _init_(CashWidthDrwal,BalanceEnquiry):
+        CashWidthDrwal=CashWidthDrwal
+        BalanceEnquiry=BalanceEnquiry
+
+    atm=atm("CashWidthDrwal","BalanceEnqiry")
+    
